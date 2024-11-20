8f50a284f3a92cbc0c1f4d5a35b032d99c6f2e62
PandaTrumpet <dsavontrumpet@gmail.com>
2024-11-20 22:26:43 +0100

    create NavBar

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,44 @@
 import { assets } from "../../assets/assets.js";
 import css from "./NavBar.module.css";
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
+import { useState } from "react";
 const NavBar = () => {
-  const buildLinkClass = ({ isActive }) => {
-    return clsx(css.link, isActive && css.active);
-  };
+  const [menu, setMenu] = useState("home");
   return (
     <nav className={css.navBar}>
       <img src={assets.logo} alt="logo" className={css.logo} />
       <ul className={css.navBarMenu}>
-        <NavLink className={buildLinkClass}>Home</NavLink>
-        <NavLink className={buildLinkClass}>Menu</NavLink>
-        <NavLink className={buildLinkClass}>Mobile-App</NavLink>
-        <NavLink className={buildLinkClass}>Contact Us</NavLink>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? css.active : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? css.active : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? css.active : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? css.active : ""}
+        >
+          contact us
+        </li>
       </ul>
       <div className={css.navBarRight}>
         <img src={assets.search_icon} alt="Search_Icon" />
         <div className={css.navBarSearchIcon}>
           <img src={assets.basket_icon} alt="" />
           <div className={css.dot}></div>
-          <button>Sign In</button>
         </div>
+        <button>Sign In</button>
       </div>
     </nav>
   );
