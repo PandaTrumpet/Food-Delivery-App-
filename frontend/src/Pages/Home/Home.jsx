0001eced222b394725e1ae5b11a63af8723a11e0
PandaTrumpet <dsavontrumpet@gmail.com>
2024-11-25 23:03:02 +0100

    ceate app-download

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu.jsx";
 import Header from "../../Components/Header/Header.jsx";
 import "./Home.css";
 import FoodDisplay from "../../Components/FoodDisplay/FoodDisplay.jsx";
+import AppDownload from "../../Components/AppDownload/AppDownload.jsx";
 const Home = () => {
   const [category, setCategory] = useState("All");
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
