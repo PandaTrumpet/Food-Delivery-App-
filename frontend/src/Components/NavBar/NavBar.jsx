c08d7f2a70db137b24e9c85c144f39198128944a
PandaTrumpet <dsavontrumpet@gmail.com>
2024-11-27 21:43:09 +0100

    add responsive style

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,36 +1,41 @@
 import { assets } from "../../assets/assets.js";
 import css from "./NavBar.module.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 const NavBar = () => {
   const [menu, setMenu] = useState("home");
   return (
     <nav className={css.navBar}>
       <img src={assets.logo} alt="logo" className={css.logo} />
       <ul className={css.navBarMenu}>
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("home")}
           className={menu === "home" ? css.active : ""}
         >
           home
-        </li>
-        <li
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? css.active : ""}
         >
           menu
-        </li>
-        <li
+        </a>
+        <a
+          href="#app-download"
           onClick={() => setMenu("mobile-app")}
           className={menu === "mobile-app" ? css.active : ""}
         >
           mobile-app
-        </li>
-        <li
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("contact-us")}
           className={menu === "contact-us" ? css.active : ""}
         >
           contact us
-        </li>
+        </a>
       </ul>
       <div className={css.navBarRight}>
         <img src={assets.search_icon} alt="Search_Icon" />
