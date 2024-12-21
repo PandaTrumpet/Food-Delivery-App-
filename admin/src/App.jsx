43ce1845063badcb2f927f711e0cbc650b687a94
PandaTrumpet <dsavontrumpet@gmail.com>
2024-12-21 22:43:20 +0100

    created admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,17 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import Add from "../src/Pages/Add/Add.jsx";
 import List from "../src/Pages/List/List.jsx";
 import Orders from "../src/Pages/Orders/Orders.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
+  const url = "http://localhost:4000";
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/orders" element={<Orders />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
