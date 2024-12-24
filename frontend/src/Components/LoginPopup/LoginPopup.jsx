3e24727aa956996f7447ecfbc9e533eb1185be93
PandaTrumpet <dsavontrumpet@gmail.com>
2024-12-24 13:46:14 +0100

    create logic for login in frontend

--- frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./LoginPopup.css";
 import { assets } from "../../assets/assets.js";
-
+import { StoreContext } from "../../context/StoreContext.jsx";
+import axios from "axios";
 const LoginPopup = ({ setShowLogin }) => {
+  const { url, setToken } = useContext(StoreContext);
   const [currState, setCurrState] = useState("Sign Up");
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeNadler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+  const onLogin = async (e) => {
+    e.preventDefault();
+    let newUrl = url;
+    if (currState === "Login") {
+      newUrl += "/api/user/login";
+    } else {
+      newUrl += "/api/user/register";
+    }
+    const response = await axios.post(newUrl, data);
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
+    } else {
+      alert(response.data.message);
+    }
+  };
   return (
     <div className="login-popup">
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <img
@@ -19,13 +50,36 @@ const LoginPopup = ({ setShowLogin }) => {
           {currState === "Login" ? (
             <></>
           ) : (
-            <input type="text" placeholder="Your name" required />
+            <input
+              type="text"
+              name="name"
+              onChange={onChangeNadler}
+              value={data.name}
+              placeholder="Your name"
+              required
+            />
           )}
 
-          <input type="email" placeholder="Your email" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            type="email"
+            name="email"
+            onChange={onChangeNadler}
+            value={data.email}
+            placeholder="Your email"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={onChangeNadler}
+            value={data.password}
+            placeholder="Password"
+            required
+          />
         </div>
-        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <button type="submit">
+          {currState === "Sign Up" ? "Create account" : "Login"}
+        </button>
         <div className="login-poppup-condition">
           <input type="checkbox" required />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,15 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets.js";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
-      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));cd 
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
   };
 
@@ -30,7 +31,11 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   };
-
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
   const contextValue = {
     food_list,
     cartItems,
@@ -38,6 +43,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
   };
   return (
     <StoreContext.Provider value={contextValue}>
