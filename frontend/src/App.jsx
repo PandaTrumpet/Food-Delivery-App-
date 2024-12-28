c94068cd48b5fbc0c517705e2de5e525f590b514
PandaTrumpet <dsavontrumpet@gmail.com>
2024-12-28 15:05:22 +0100

    create logic pay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar.jsx";
 import Home from "./Pages/Home/Home.jsx";
-
+import Verify from "./Pages/Verify/Verify.jsx";
 import "./index.css";
 import Cart from "./Pages/Cart/Cart.jsx";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import { useState } from "react";
 import LoginPopup from "./Components/LoginPopup/LoginPopup.jsx";
+
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer />
